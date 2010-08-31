b86dc0ec25d051615a20cd5ba9b375ef083100b2
Ingy dot Net <ingy@ingy.net>
2010-08-30 19:52:53 -0700

    MOved grammar to YAML3::Parser::Grammar

---
+++ lib/YAML3/Parser/Grammar.pm
@@ -0,0 +1,39 @@
+package YAML3::Parser::Grammar;
+use Pegex::Grammar -base;
+
+sub grammar_text {
+    return <<'...';
+stream: <document>*
+
+document: <directive>* <header> <node>
+
+directive: /<PERCENT><WORD>+<SPACE>+(<ANY>+)<EOL>/
+
+header: /<DASH><DASH><DASH>/
+
+node: <seq> | <map> | <scalar>
+
+seq: <empty_seq> | [ /<EOL>/ <item>+ ]
+
+empty_seq: /<LSQUARE><RSQUARE><EOL>/
+
+item: /<DASH><SPACE>+/ <value> /<EOL>/
+
+map: <empty_map> | [ /<EOL>/ <pair>+ ]
+
+empty_map: /<LCURLY><RCURLY><EOL>/
+
+pair: <key> /<SPACE>*<COLON><SPACE>+/ <value> /<EOL>/
+
+# value is kinda bogus, but the tests will guide us.
+
+key: /(<ANY>+)(?=<SPACE>*<COLON>)/
+
+value: /(<ANY>*)/
+
+scalar: /<SPACE>+/ <value> /<EOL>/
+
+...
+}
+
+1;
